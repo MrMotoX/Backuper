391925893b4bb961b6fe585bbb5d2023f49db141
Rickard <rickard.fransen@gmail.com>
2017-09-28 22:47:05 +0200

    Made a folder picker to support the option of selecting a folder to zip.

--- Backuper/MainWindow.xaml.cs
+++ Backuper/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.IO.Compression;
 
 namespace Backuper
 {
@@ -20,9 +23,38 @@ namespace Backuper
     /// </summary>
     public partial class MainWindow : Window
     {
+        private FolderBrowserDialog folderBrowserDialog1;
+
+        private string folderName;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.folderBrowserDialog1 = new FolderBrowserDialog();
+            this.folderBrowserDialog1.Description = "Select the directory that you want to use as the default.";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            this.folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Personal;
+        }
+
+        public void CreateZipDirectory()
+        {
+            string startPath = @"c:\example\start";
+            string zipPath = @"c:\example\result.zip";
+            string extractPath = @"c:\example\extract";
+
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+
+        private void ShowFolderDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                ChosenFolderTextBox.Text = folderBrowserDialog1.SelectedPath;
+            }
         }
     }
 }
