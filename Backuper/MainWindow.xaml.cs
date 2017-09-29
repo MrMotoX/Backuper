d4f345eb49de7a2ed9e3612dc24f9838a7f97ddb
Rickard <rickard.fransen@gmail.com>
2017-09-30 01:41:35 +0200

    Fortsätter testa funktionerna för att skapa en zip-fil.

--- Backuper/MainWindow.xaml.cs
+++ Backuper/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Forms;
-using System.IO.Compression;
+using System.IO;
 
 namespace Backuper
 {
@@ -11,35 +11,62 @@ namespace Backuper
     public partial class MainWindow : Window
     {
         private FolderBrowserDialog folderBrowserDialog1;
-        private string folderName;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            this.folderBrowserDialog1 = new FolderBrowserDialog();
-            this.folderBrowserDialog1.Description = "Select the directory that you want to use as the default.";
-            this.folderBrowserDialog1.ShowNewFolderButton = false;
-            this.folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog1 = new FolderBrowserDialog()
+            {
+                ShowNewFolderButton = true,
+                RootFolder = Environment.SpecialFolder.MyComputer
+            };
         }
 
-        public void CreateZipDirectory()
+        private void PickDirectoryButton_Click(object sender, RoutedEventArgs e)
         {
-            string startPath = @"c:\example\start";
-            string zipPath = @"c:\example\result.zip";
-            string extractPath = @"c:\example\extract";
+            string tag = ((System.Windows.Controls.Button)sender).Tag.ToString();
+            if (tag == "base")
+            {
+                folderBrowserDialog1.Description = "Välj den mapp du vill komprimera.";
+            }
+            else if (tag == "target")
+            {
+                folderBrowserDialog1.Description = "Välj den mapp du vill skapa den komprimerade filen i.";
+            }
 
-            ZipFile.CreateFromDirectory(startPath, zipPath);
-
-            ZipFile.ExtractToDirectory(zipPath, extractPath);
-        }
-
-        private void ShowFolderDialogButton_Click(object sender, RoutedEventArgs e)
-        {
             DialogResult result = folderBrowserDialog1.ShowDialog();
+
             if (result == System.Windows.Forms.DialogResult.OK)
             {
-                ChosenFolderTextBox.Text = folderBrowserDialog1.SelectedPath;
+                if (tag == "base")
+                {
+                    ZipArchiver.SetBaseDirectory(folderBrowserDialog1.SelectedPath);
+                    BaseDirectoryString.Text = folderBrowserDialog1.SelectedPath;
+                    StatusString.Text = "Basmapp vald";
+                }
+                else if (tag == "target")
+                {
+                    ZipArchiver.SetTargetDirectory(folderBrowserDialog1.SelectedPath);
+                    TargetDirectoryString.Text = folderBrowserDialog1.SelectedPath;
+                    StatusString.Text = "Målmapp vald";
+                }
+            }
+        }
+
+        private void CreateZipButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ZipArchiver.CreateBackup();
+            }
+            catch (ArgumentNullException)
+            {
+                StatusString.Text = "Du måste välja en bas- och en målmapp";
+            }
+            catch (DirectoryNotFoundException)
+            {
+                StatusString.Text = "Du har valt en mapp som inte finns";
             }
         }
     }
---
+++ Backuper/ZipArchiver.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Backuper
+{
+    static class ZipArchiver
+    {
+        private static string baseDirectory;
+        private static string targetDirectory;
+
+        public static void SetBaseDirectory(string folder)
+        {
+            baseDirectory = folder;
+        }
+
+        public static void SetTargetDirectory(string folder)
+        {
+            targetDirectory = folder;
+        }
+
+        public static void CreateBackup()
+        {
+            string fileName = Path.GetDirectoryName(targetDirectory) + ".zip";
+            ZipFile.CreateFromDirectory(baseDirectory, @"C:\Installation\Siemens\gsd.zip");
+        }
+    }
+}
